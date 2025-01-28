34f4d02a7204cf3beb5468854e0223141ee01d24
Riquelmi Santos <riquelme.dos.santos.kw@gmail.com>
2025-01-28 01:50:51 -0300

    feat: Alteracao na logica da navbar com codigo comentado

--- produtosServicos/ProdutosServicos.js
+++ produtosServicos/ProdutosServicos.js
@@ -1,22 +1,53 @@
-let nav = document.getElementById('nav-mobile');
+const navContainer = document.getElementById('nav-mobile');
+const btnBurguerNav = document.getElementById('btn-burguer');
+const nav = document.querySelector('nav');
+
+let navBarTaAberta = false;
 
 function navMobileMostrar() {
-    nav.style.display = 'block';
-    document.addEventListener('click', (event) => {
-        if (event.target !== nav && !nav.contains(event.target)){
-            ocultarNav();
-        }
-    });
-    document.addEventListener('touchend', (event) => {
-        if (event.target !== nav && !nav.contains(event.target)) {
-          ocultarNav();
-        }
-    });
+
+    if (navBarTaAberta) return; // Se a nav ja estiver aberta saia da funcao
+
+    //* Dica: Toda vez que voce usar um return , c ta dizendo que a funcao vai acabar ali pois toda funcao tem um return implicito no final
+
+    navBarTaAberta = true; // Cliquei no btn-burguer e "boom" nav bar agr ta aberta, logo atualiza o valor da varivel 
+
+    navContainer.style.display = 'block';
+
+    btnBurguerNav.style.transform = 'rotate(90deg)'; // efeitozinho de virar o btn-burguer se n gostar pode tirar kk
+
+    document.addEventListener('click', verificaCliqueForaTela ); // Evento de clique em qualquer lugar da tela chama a funcao verificaCliqueForaTela
+    
+    document.addEventListener('touchend', verificaCliqueForaTela);
+}
+
+
+
+function verificaCliqueForaTela(event) { 
+
+    //*Dica: Toda vez que uma funcao dentro de um eventListeiner receber um parametro exemplo "event", ele recebe um "objeto" com contendo varias informacoes sobre o evento que aconteceu
+
+
+    // Se o elemento que disparou o evento for diferente do btn burguer ou de qualquer elemento presente na tag nav, feche a navbar
+    if (event.target !== btnBurguerNav && !nav.contains(event.target)) {
+        ocultarNav();
+    }
+
+    //*Dica: event.target é o elemento que disparou o evento
+    //* Olha no console o que acontece quando vc clica fora e dentro do botao
+    console.log(event.target);
+    
 }
 
 function ocultarNav() {
-    nav.style.display = 'none';
-    document.removeEventListener('click', ocultarNav);
-    document.removeEventListener('touchend', ocultarNav);
+
+    navContainer.style.display = 'none';
+    navBarTaAberta = false; // Navbar ta fechada entao atualiza o valor da varivel
+
+    btnBurguerNav.style.transform = 'rotate(0deg)';
+
+    //remove os eventos adicionados
+    document.removeEventListener('click', verificaCliqueForaTela);
+    document.removeEventListener('touchend', verificaCliqueForaTela);
 }
 
